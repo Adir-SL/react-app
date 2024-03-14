b7570b6d1f3f7dadde45ff6cb0eed016881436e9
Adir <slutzkis@gmail.com>
2024-03-14 14:49:56 +0200

    updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import "./Login.css";
 import Logo from "./assets/mlrun.png";
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 const Login = (props) => {
     return (
         <div className='comp-login'>
-            <div className="logo"><img src={Logo} /></div>
-            <Input header="Enter your credentials" placetext="User Name" />
-            <Input placetext="Password" />
+            <div className="login-flex">
+                <div className="logo"><img src={Logo} /></div>
+                <Input header="Enter your credentials" placetext="User Name" />
+                <Input placetext="Password" />
+                <Button label="Login" />
+            </div>
         </div>
     )
 };
