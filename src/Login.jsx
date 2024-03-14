d07072b4811133b87f3bf5e53503e3284ecce83a
Adir <slutzkis@gmail.com>
2024-03-14 14:03:33 +0200

    updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import React from 'react';
 import "./Login.css";
+import Logo from "./assets/mlrun.png";
+import Input from "./components/Input";
 
 const Login = (props) => {
     return (
         <div className='comp-login'>
-            LOGIN
+            <div className="logo"><img src={Logo} /></div>
+            <Input header="Enter your credentials" placetext="User Name" />
+            <Input placetext="Password" />
         </div>
     )
 };
