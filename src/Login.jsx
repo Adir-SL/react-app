4bab71fdff37614b1411a653112765a2ae1e6504
Adir <slutzkis@gmail.com>
2024-03-14 20:42:54 +0200

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,18 +4,25 @@ import "./Login.css";
 import Logo from "./assets/mlrun.png";
 import Input from "./components/Input";
 import Button from "./components/Button";
+import {AppContext} from "./services/AppContext";
 
 const Login = (props) => {
+    const [username, setUsername] = useState(AppContext);
+
     return (
         <div className='comp-login'>
             <div className="login-flex">
                 <div className="logo"><img src={Logo} /></div>
-                <Input type="text" header="Enter your credentials" content="User Name" placetext="Enter your user name" />
+                <Input type="text" header="Enter your credentials" content="User Name" placetext="Enter your user name" onChange={() => {setUsername('Adam');}} />
                 <Input type="password" content="Password" placetext="Enter your password" />
-                <Button label="Login" onClick={() => {props.onlogin(false)}} />
+                <Button label="Login" onClick={() => {
+                    console.log(username);
+                    props.onlogin(false);
+                }} />
             </div>
         </div>
     )
 };
+// onClick={() => setText('Hello, world!')}
 
 export default Login;
