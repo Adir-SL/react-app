ef07bfa6c410a1b6a4318b25e8fc6079c5faaa06
Adir <slutzkis@gmail.com>
2024-03-20 12:31:24 +0200

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,22 +5,32 @@ import "./Login.css";
 import Logo from "./assets/mlrun.png";
 import Input from "./components/Input";
 import Button from "./components/Button";
-import {AppContext} from "./services/AppContext";
+import { AppContext } from "./services/AppContext";
 
 const Login = (props) => {
-    const {sessionId, setSessionId, username, setUsername} = useContext(AppContext);
+    const { sessionId, setSessionId, username, setUsername } = useContext(AppContext);
+    const [FailedToLoad, setFailedToLoad] = useState(false);
 
     return (
         <div className='comp-login'>
             <div className="login-flex">
+                {FailedToLoad ? (
+                    <div>Failed</div>
+                ) : (
+                    <div></div>
+                )}
                 <div className="logo"><img src={Logo} /></div>
                 <Input type="text" header="Enter your credentials" content="User Name" placetext="Enter your user name" />
                 <Input type="password" content="Password" placetext="Enter your password" />
                 <Button label="Login" onClick={() => {
                     setUsername(document.querySelectorAll('input')[0].value);
-                    setTimeout(function(){
-                        props.onlogin(false);
-                    }, 200);
+                    if (document.querySelectorAll('input')[0].value == 'fail') {
+                        setFailedToLoad(true);
+                    } else {
+                        setTimeout(function () {
+                            props.onlogin(false);
+                        }, 200);
+                    }
                 }} />
             </div>
         </div>
