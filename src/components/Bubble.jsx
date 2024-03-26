18b6e88c431a1c5e3cc659c47729e60d2b29da1f
Adir <slutzkis@gmail.com>
2024-03-26 15:01:07 +0200

    updates

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -5,6 +5,25 @@ import IconBot from "./IconBot";
 import IconHuman from "./IconHuman";
 import Sources from "./Sources";
 import Markdown from 'react-markdown';
+import { createGrid } from 'ag-grid-community';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+
+const gridOptions = {
+	columnDefs: [
+		{ headerName: 'Make', field: 'make' },
+		{ headerName: 'Model', field: 'model' },
+		{ headerName: 'Price', field: 'price' }
+	],
+	rowData: [
+		{ make: 'Toyota', model: 'Celica', price: 35000 },
+		{ make: 'Ford', model: 'Mondeo', price: 32000 },
+		{ make: 'Porsche', model: 'Boxster', price: 72000 }
+	]
+};
+const eGridDiv = document.querySelector('#myGrid');
+const api = createGrid(eGridDiv, gridOptions);
 
 const Bubble = (props) => {
     return (
@@ -17,6 +36,7 @@ const Bubble = (props) => {
                 <div className='inner-bubble'>
                     <Markdown>{props.content}</Markdown>
                     <div className='html-code' dangerouslySetInnerHTML={{__html: props.html}}></div>
+                    <div id="myGrid" style="height: 150px; width: 600px" class="ag-theme-quartz"></div>
                 </div>
                 {props.bot == 'AI' ? <div className='help-text'>Was this helpful? <button>👍</button><button>👎</button></div> : <div className='help-text'></div>}
 
