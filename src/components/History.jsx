747c3058d48f93b991f107ddab8d06d704d8e48b
Adir <slutzkis@gmail.com>
2024-04-10 15:13:03 +0300

    updates

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,20 +4,22 @@ import "./History.css";
 
 const History = (props) => {
     const histories = props.history;
-    
+
     return (
         <div className='comp-history'>
 
             <div className="inner-flex">
                 {histories.map((history) => (
-                    <button onClick={() => props.onSelect(history.name)}>
-                        {history.description || history.name}
+                    <button>
+                        <button onClick={() => props.onSelect(history.name)}>
+                            {history.description || history.name}
+                        </button>
                         <details>
                             <summary>
                                 <div className="icon-button"></div>
                             </summary>
                             <div className="menu">
-                            <div className="menu-item">
+                                <div className="menu-item">
                                     <div className="menu-icon rename"></div>
                                     Rename
                                 </div>
