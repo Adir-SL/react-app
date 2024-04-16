849027214395e97c68da00e8dc03a4342646614d
Adir <slutzkis@gmail.com>
2024-04-16 13:05:27 +0300

    updates

--- src/admin/components/Table.jsx
+++ src/admin/components/Table.jsx
@@ -46,7 +46,19 @@ const Table = (props) => {
 
     return (
         <div className={"comp-table"}>
-            <iframe className="drilldown" name="drill" id="drillFrame"></iframe>
+            <div className="drilldown" id="drillFrame">
+            <button className='close-button'
+                onClick={() => {
+                    document.getElementById("drillFrame").classList.remove('shown');
+                }}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+
+            </button>
+            <iframe name="drill"></iframe>
+            </div>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={colDefs}
