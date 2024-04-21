88f9d6a26c892bc9305aaa63587f27eccc8596b9
Adir <slutzkis@gmail.com>
2024-04-21 12:02:05 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import React from 'react';
 import "./Modal.css";
+import { AppContext } from "../services/AppContext";
 
 const Modal = (props) => {
+    const { modal, setModal } = useContext(AppContext);
     return (
         <div className='comp-modal'>
             <div className="modal">
                 <h2>{props.title}</h2>
                 <p>{props.content}</p>
                 <footer>
-                    <button>Cancel</button>
+                    <button className='second-button' onClick={() => { setModal(false) }}>Cancel</button>
                     <button>OK</button>
                 </footer>
             </div>
