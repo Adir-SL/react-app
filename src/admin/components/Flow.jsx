0c7ddec0e2976947c0784a419856762b17859ea5
Adir <slutzkis@gmail.com>
2024-05-01 10:31:49 +0300

    updates

--- src/admin/components/Flow.jsx
+++ src/admin/components/Flow.jsx
@@ -10,20 +10,9 @@ import ReactFlow, {
 
 import 'reactflow/dist/style.css';
 
-const initialNodes = [
-    { id: '1', position: { x: 50, y: 50 }, data: { label: 'item-1' } },
-    { id: '2', position: { x: 150, y: 150 }, data: { label: 'item-2' } },
-    { id: '3', position: { x: 50, y: 250 }, data: { label: 'item-3' } },
-];
-
-const initialEdges = [
-    { id: 'e1-2', source: '1', target: '2' },
-    { id: 'e2-3', source: '2', target: '3' }
-];
-
 const Flow = (props) => {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const [nodes, setNodes, onNodesChange] = useNodesState(props.initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(props.initialEdges);
 
     const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
     const proOptions = { hideAttribution: true };
